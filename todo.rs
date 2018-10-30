0c4086779cafc875c9c1ca017b9fea4967f494c5
Doug Alpuche <m190108@usna.edu>
2018-10-30 08:34:03 -0400

    Initialized program file

---
+++ todo.rs
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+
+
+fn main(){
+
+    let filename = #argv1;
+    //If filename exists, open it for reading and writing, else error
+    //Else create file... notify
+
+    //Spawn 2 Threads...
+    //Thread 1 -> Main UI
+    /*
+     * checkArgs() -> Looks if a filename was given, if not prompt for one...
+     * mainMenu()  -> Display a basic prompt (Readline repl loop)
+     * showHelp()  -> Display a list of commands
+     * printList() -> Display the current to do list
+     * toggleListDisplay() -> Permanently display the list above the prompt: toggleable^
+     * 
+     */
+    //Thread 2 -> FILE I/O
+    /*
+     * updateFile() -> Update the to do list, called by thread one each time
+     * checkForUpdates() -> Look if another terminal/application updated the to do list file
+     * 
+     */
+}
